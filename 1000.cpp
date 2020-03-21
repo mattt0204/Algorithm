dba2d16bcc03eb00edf013d5308581bf7e64bdea
Neo0204 <wootaejung24@gmail.com>
2020-03-21 15:57:56 +0900

    init

---
+++ 1000.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> a >> b;
+    cout << a + b;
+    return 0;
+}
---
+++ 10869.cpp
@@ -0,0 +1,20 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a + b;
+    cout << "\n";
+    cout << a - b;
+    cout << "\n";
+    cout << a * b;
+    cout << "\n";
+    cout << a / b;
+    cout << "\n";
+    cout << a % b;
+
+    return 0;
+}
